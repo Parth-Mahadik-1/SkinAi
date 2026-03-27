bbdbf9b61c31b7ab3233bd665526af00d713f52e
MR.Parth mahadik <parthmahadik44@gmail.com>
2026-03-27 09:00:44 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -31,8 +31,8 @@ BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
 app = Flask(
     __name__,
-    template_folder=os.path.join(BASE_DIR, "..frontend/templates"),
-    static_folder=os.path.join(BASE_DIR, "../frontend/static")
+    template_folder=os.path.join(BASE_DIR, "templates"),
+    static_folder=os.path.join(BASE_DIR, "static")
 )
 
 app.secret_key = "skinai_secret_key"
